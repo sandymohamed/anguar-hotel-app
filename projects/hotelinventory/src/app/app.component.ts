ea55e2b1aea4c389c2ad68d3c4a69e0237a4baa4
sandymohamede2 <sandymohamede2@gmail.com>
2023-10-14 06:12:05 +0300

    add interface pipe

--- projects/hotelinventory/src/app/app.component.ts
+++ projects/hotelinventory/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+// styles: [`h1 {color: blue}`]
 })
 export class AppComponent {
   title = 'hotelinventory';
+
+  role = "Admin";
 }
--- projects/hotelinventory/src/app/rooms/rooms.component.ts
+++ projects/hotelinventory/src/app/rooms/rooms.component.ts
@@ -1,24 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import { Room, RoomList } from '../interfaces/room';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent implements OnInit{
+export class RoomsComponent implements OnInit {
 
-  hotelName:String = "Hilton Hotel";
+  hotelName: String = "Hilton Hotel";
   numberOfRooms = 10;
-  hideRooms =true;
+  hideRooms = true;
 
-  constructor() {}
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 15,
+    bookedRooms: 5,
+  };
+
+  roomList: RoomList[] = [
+    {
+      roomNumber:1,
+      roomType: "Deluxe Room",
+      amenities: "Air Conditioner, free wi-fi, TV, Bathroom, Kitchen",
+      price: 500,
+      rating: 4.5,
+      photos: "",
+      checkinTime: new Date("11-Nov-2023"),
+      checkoutTime: new Date("12-Nov-2023"),
+    },
+    {
+      roomNumber:2,
+      roomType: "Deluxe Room",
+      amenities: "Air Conditioner, free wi-fi, TV, Bathroom, Kitchen",
+      price: 700,
+      rating: 2.2,
+      photos: "",
+      checkinTime: new Date("21-Nov-2023"),
+      checkoutTime: new Date("25-Nov-2023"),
+    },
+    {
+      roomNumber:3,
+      roomType: "Private Suits",
+      amenities: "Air Conditioner, free wi-fi, TV, Bathroom, Kitchen",
+      price: 900,
+      rating: 3.5,
+      photos: "",
+      checkinTime: new Date("18-Nov-2023"),
+      checkoutTime: new Date("20-Nov-2023"),
+    },
+    {
+      roomNumber:4,
+      roomType: "Private Suits",
+      amenities: "Air Conditioner, free wi-fi, TV, Bathroom, Kitchen",
+      price: 1500,
+      rating: 4.7,
+      photos: "",
+      checkinTime: new Date("11-Dec-2023"),
+      checkoutTime: new Date("12-Dec-2023"),
+    },
+  ]
+
+  constructor() { }
 
   ngOnInit(): void {
-      
+
   }
 
   toggle() {
-    this.hideRooms =!this.hideRooms;
+    this.hideRooms = !this.hideRooms;
   }
 
 }
